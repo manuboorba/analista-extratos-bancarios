0b32588d2803e380e51ae69f4be7e0b90d53f153
Manuela <manuelacavalcanti3@gmail.com>
2024-12-19 11:53:21 -0300

    Criando as classes BankTransaction e BankStatementCSVParser

---
+++ src/main/learning/chapter02/BankTransaction.java
@@ -0,0 +1,33 @@
+package main.learning.chapter02;
+
+import java.time.LocalDate;
+
+public class BankTransaction {
+    private final LocalDate date;
+    private final double amount;
+    private final String description;
+
+
+    public BankTransaction(final LocalDate date, final double amount, final String description) {
+        this.date = date;
+        this.amount = amount;
+        this.description = description;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public double getAmount(){
+        return amount;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return "BankTransaction{" + "date=" + date + ", amount=" + amount + ", description=" + description + ''
+    }
+}
